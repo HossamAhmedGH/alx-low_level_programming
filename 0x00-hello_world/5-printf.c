b5b4b852b7df8a5bb33a9657aeba3ed23945fa59
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-08-10 14:23:15 -0700

    dfasdfa

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ * main - just a code that prints a phrase
+ *return 0 if correct, otherwise, something else, idk
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
