2ff0cd3e962cd6f1d2459c1aaca9626bfd3a091d
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-08-11 16:38:20 -0700

    sdfasdfaf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("The number is %d and it's positive", n)
+		printf("The number is %d and it's positive", n);
 	else if (n == 0)
-		printf("The number is %d, zilch, nada", n)
-	else if(n < 0)
-		printf("The number is %d and it's negative, n")
+		printf("The number is %d, zilch, nada", n);
+	else if (n < 0)
+		printf("The number is %d and it's negative", n);
 	return (0);
 }
