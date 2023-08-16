abe361f503059b93859eb1bbc07e6c1137d723aa
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-08-15 17:37:32 -0700

    dsfasdfaa

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int nn = 1;
+
+	while (nn <= 10)
+		{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		}
+		nn++;
+}
