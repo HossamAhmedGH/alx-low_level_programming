8abeab33338fb26dc86189950115feae1ad7992c
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-08-17 07:04:43 -0700

    fasdfa

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - as the name might imply, it actually prints numbers!
+ * Return: returns the numbers.
+ */
+
+void print_numbers(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+	_putchar(x);
+	x++;
+	}
+	_putchar('\n');
+}
