184fedd5f01a7b586dd6b994e56a691badf43a06
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-08-17 15:06:57 -0700

    dsafasdfa

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers, obviously!
+ * btw, for 10 times and adds a line.
+ */
+void more_numbers(void)
+	{
+	int x = 1;
+
+	while (x <= 10)
+		{
+		int y = 0;
+
+		while (y <= 14)
+			{
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			y++;
+			}
+		_putchar('\n');
+		x++;
+		}
+	}
