027fe27c5e7738578237901f638867d9a71bd846
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-13 04:11:26 -0700

    asdfaf

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - don't care....
+ * Return: void.
+ * @a: the number to be edited.
+ * @b: variable.
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
