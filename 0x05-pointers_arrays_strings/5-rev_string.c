47a2ef346119c1b8b5805442e1ece02d8e5f6e4e
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-13 14:44:39 -0700

    asdfaf

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - revs a string....
+ * Return: void.
+ * @s: the string to work with.
+*/
+
+void rev_string(char *s)
+{
+	int a;
+	int b;
+	int c;
+	int d = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	for (b = a - 1; b > a / 2; b--)
+	{
+		c = s[d];
+		s[d] = s[b];
+		s[b] = c;
+		d++;
+	}
+}
+
