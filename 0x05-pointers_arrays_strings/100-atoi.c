50600fbaadb752e98c4d7f45d2a775536a9f7b04
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-14 08:11:13 -0700

    sadfaf

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,7 @@ int _atoi(char *s)
 {
 	int a; /*length of the string*/
 	int b; /*used to increment until the end of the string*/
-	int c = 0; /*number of positive signs*/
-	int d = 0; /*number of negative signs*/
+	int c = 1; /*used to determine the sign*/
 	int e; /*increments in the exctarction loop*/
 	int the_integer = 0; /*the integer to be saved in the return value*/
 
@@ -19,29 +18,33 @@ int _atoi(char *s)
 	;
 	for (b = 0; b <= a; b++)
 	{
-		if (s[b] == '+') /*for the positive sign*/
+		if (s[b] == '-')
 		{
-			c++;
+			c *= -1;
 		}
-		else if (s[b] == '-') /*for the negative sign*/
+		else if (s[b] == '+')
 		{
-			d++;
+			c *= 1;
 		}
 	} /*now i have the number of positive and negative signs to use with return*/
 	for (e = 0; e <= a; e++)
 	{
+		if (s[e] == ';')
+		{
+			break;
+		}
 		if (s[e] >= '0' && s[e] <= '9')
 		{
 			the_integer = (the_integer * 10) + (s[e] - '0');
 		}
 	}
-	if (d > c)
+	if (c == -1)
 	{
 		return (-the_integer);
 	}
 	else
 	{
-		return (the_integer);
+	return (the_integer);
 	}
 }
 
