73e3a6a60e16bf7e8f6e0c9d1f5a98e02db65bbf
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-15 09:09:59 -0700

    asdfafa

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversed a string.
+ * Return: void.
+ * @a: a pointer.
+ * @n: an integer.
+*/
+
+void reverse_array(int *a, int n)
+{
+	int d;
+	int e;
+	int c = n;
+
+	for (d = 0; d < n / 2; d++)
+	{
+		e = a[d];
+		a[d] = a[c];
+		a[c] = e;
+		c--;
+	}
+}
+
