fba637e1b4c0aebbbdc3ce3e76263d15daffda4c
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-26 14:33:54 -0700

    asdfadfadf

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
@@ -9,7 +10,6 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
-	int diff;
 	listint_t *temp;
 
 	if (!h || !*h)
@@ -17,19 +17,10 @@ size_t free_listint_safe(listint_t **h)
 
 	while (*h)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
+		len++;
 	}
 
 	*h = NULL;
