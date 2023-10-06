1d892535d1e50a68429cc3aa99c98f4f42cfce55
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-10-06 12:27:40 -0700

    sffhsfghs

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets da string.
+ * @s: double pointer to the targetted chars.
+ * @to: the destination.
+ * Return: void.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
